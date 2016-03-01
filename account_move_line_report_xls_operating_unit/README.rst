f1d1ffb4e8b2f3c567f0d666b8af8ab4233f6c66
Andre Schenkels <a.schenkels@ictstudio.eu>
2016-03-01 15:37:32 +0000

    [ADD][OPERATING UNIT] Account Move Line Report XLS for Operating Unit
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@26 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ account_move_line_report_xls_operating_unit/README.rst
@@ -0,0 +1,6 @@
+Journal Items Excel Export
+==========================
+
+This module adds the following fields to the journal items export:
+
+- Operating Unit
---
+++ account_move_line_report_xls_operating_unit/models/account_move_line.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2009-2016 Noviat nv/sa (www.noviat.com).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api, models, _
+from openerp.addons.report_xls.utils import _render
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    @api.model
+    def _report_xls_fields(self):
+        res = super(AccountMoveLine, self)._report_xls_fields()
+        ix = res.index('account')
+        res.insert(ix + 1, 'operating_unit_name')
+        return res
+
+    @api.model
+    def _report_xls_template(self):
+        update = {
+            'operating_unit_name': {
+                'header': [1, 25, 'text', _('Operating Unit')],
+                'lines': [
+                    1, 0, 'text', _render(
+                        "line.account_id.operating_unit_id and "
+                        "line.account_id.operating_unit_id.name "
+                        "or ''")],
+                'totals': [1, 0, 'text', None]},
+        }
+        return update
