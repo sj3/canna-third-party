0308a76bf6b78b3a7f251e42df8c9f0dee51f430
Simon Janssens <s.janssens@onestein.nl>
2016-09-13 09:59:00 +0000

    [ADD] sale_order_purchase_reference
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@121 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ sale_order_purchase_reference/README.rst
@@ -0,0 +1,7 @@
+Sale Order Purchase Reference
+=============================
+
+This module add a button on the sale order to view
+the related (mto) purchases.
+
+
---
+++ sale_order_purchase_reference/models/sale_order.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2015 Onestein BV (www.onestein.eu).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api, fields, models, _
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    purchase_order_ids = fields.Many2one(
+        comodel_name='purchase.order',
+        compute='_compute_purchase_order_ids',
+        string='Purchase Orders',
+    )
+    purchase_order_count = fields.Integer(
+        compute='_compute_purchase_order_ids',
+        string='# of Sales Order'
+    ) 
+    @api.one
+    def _compute_purchase_order_ids(self):
+        procs = self.env['procurement.order'].search(
+            [('sale_order_id', '=', self.id),
+             ('state', '!=', 'cancel')
+             ])
+        self.purchase_order_ids = procs.mapped('purchase_id')
+        self.purchase_order_count = len(self.purchase_order_ids)
+
+    @api.multi
+    def view_purchase_order(self):
+        self.ensure_one()
+        action = {}
+        po_ids = [x.id for x in self.purchase_order_ids]
+        if po_ids:
+            form = self.env.ref(
+                    'purchase.purchase_order_form')
+            if len(po_ids) > 1:
+                tree = self.env.ref(
+                    'purchase.purchase_order_tree')
+                action.update({
+                    'name': _('Purchase Orders'),
+                    'view_mode': 'tree,form',
+                    'views': [(tree.id, 'tree'), (form.id, 'form')],
+                    'domain': [('id', 'in', po_ids)],
+                    })
+            else:
+                action.update({
+                    'name': _('Purchase Order'),
+                    'view_mode': 'form',
+                    'view_id': form.id,
+                    'res_id': po_ids[0],
+                    })
+            action.update({
+                'context': self._context,
+                'view_type': 'form',
+                'res_model': 'purchase.order',
+                'type': 'ir.actions.act_window',
+                })
+        return action
