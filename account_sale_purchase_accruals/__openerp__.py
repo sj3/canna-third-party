5b049249fd2c712a436091550d7aad45587b992c
Luc de Meyer <luc.demeyer@noviat.com>
2016-09-20 18:37:35 +0000

    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@136 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

--- account_sale_purchase_accruals/__openerp__.py
+++ account_sale_purchase_accruals/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Account Sale Purchase Accruals',
-    'version': '8.0.0.3.3',
+    'version': '8.0.0.4.0',
     'license': 'AGPL-3',
     'author': 'Noviat',
     'website': 'http://www.noviat.com',
