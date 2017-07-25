842b8d3cb65d2006ad59667308285ac36d5a2610
Luc de Meyer <luc.demeyer@noviat.com>
2017-07-25 10:39:42 +0000

    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@284 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ account_reinvoice/models/account_move_line.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2016 Noviat
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models, _
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    reinvoice_key_id = fields.Many2one(
+        comodel_name='account.reinvoice.key',
+        string='Reinvoice Key', index=True)
+    reinvoice_line_ids = fields.One2many(
+        comodel_name='account.reinvoice.line',
+        inverse_name='move_line_in_id', readonly=True,
+        string='Reinvoice Line')
+    reinvoice_line_count = fields.Integer(
+        compute='_compute_reinvoice_line_count',
+        string='# of reinvoice lines')
+
+    @api.one
+    def _compute_reinvoice_line_count(self):
+        self.reinvoice_line_count = len(self.reinvoice_line_ids)
+
+    @api.multi
+    def view_reinvoice_lines(self):
+        self.ensure_one()
+        action = {}
+        arl_ids = [x.id for x in self.reinvoice_line_ids]
+        if arl_ids:
+            module = __name__.split('addons.')[1].split('.')[0]
+            form = self.env.ref(
+                '%s.account_reinvoice_line_form') % module
+            if len(arl_ids) > 1:
+                tree = self.env.ref(
+                    '%s.account_reinvoice_line_tree') % module
+                action.update({
+                    'name': _('Reinvoice Lines'),
+                    'view_mode': 'tree,form',
+                    'views': [(tree.id, 'tree'), (form.id, 'form')],
+                    'domain': [('id', 'in', arl_ids)],
+                    })
+            else:
+                action.update({
+                    'name': _('Reinvoice Line'),
+                    'view_mode': 'form',
+                    'view_id': form.id,
+                    'res_id': arl_ids[0],
+                    })
+            action.update({
+                'context': self._context,
+                'view_type': 'form',
+                'res_model': 'account.reinvoice.line',
+                'type': 'ir.actions.act_window',
+                })
+        return action
