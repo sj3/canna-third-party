bb8095bf18bf4b68473eed0d18b51f1425ed092e
Luc de Meyer <luc.demeyer@noviat.com>
2017-08-30 07:28:32 +0000

    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@305 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ module_list_readonly/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2017 Noviat
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Module List readonly',
+    'version': '8.0.0.0.4',
+    'license': 'AGPL-3',
+    'author': 'Noviat',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'security/module_security.xml',
+        'views/remove_sidebar_action.xml',
+        'views/ir_module_module.xml',
+    ],
+    'installable': True,
+}
