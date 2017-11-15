194d1536fe76941d7d2b57089408a25b447a632e
Luc de Meyer <luc.demeyer@noviat.com>
2017-11-15 16:35:26 +0000

    sale order ou based on invoice partner ou
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@339 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

--- sale_operating_unit/models/sale_order.py
+++ sale_operating_unit/models/sale_order.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# © 2015 Eficent Business and IT Consulting Services S.L.
+# © 2015 Serpent Consulting Services Pvt. Ltd.
+# © 2017 Noviat
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
-from openerp.exceptions import Warning
-from openerp.tools.translate import _
+
+from openerp import api, fields, models, _
+from openerp.exceptions import Warning as UserError
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
-                                        default=lambda self:
-                                        self.env['res.users'].
-                                        operating_unit_default_get(self._uid))
+    operating_unit_id = fields.Many2one(
+        comodel_name='operating.unit',
+        string='Operating Unit',
+        default=lambda self:
+        self.env['res.users'].operating_unit_default_get(self._uid))
 
     @api.one
     @api.constrains('operating_unit_id', 'company_id')
     def _check_company_operating_unit(self):
         if self.company_id and self.operating_unit_id and\
                 self.company_id != self.operating_unit_id.company_id:
-            raise Warning(_('Configuration error!\nThe Company in the\
+            raise UserError(_('Configuration error!\nThe Company in the\
             Sales Order and in the Operating Unit must be the same.'))
 
     @api.one
@@ -29,8 +30,14 @@ class SaleOrder(models.Model):
     def _check_wh_operating_unit(self):
         if self.operating_unit_id and\
                 self.operating_unit_id != self.warehouse_id.operating_unit_id:
-            raise Warning(_('Configuration error!\nThe Operating Unit \
+            raise UserError(_('Configuration error!\nThe Operating Unit \
             in the Sales Order and in the Warehouse must be the same.'))
+
+    @api.onchange('partner_invoice_id')
+    def _onchange_partner_invoice_id(self):
+        cp = self.partner_invoice_id.commercial_partner_id
+        if cp.operating_unit_id:
+            self.operating_unit_id = cp.operating_unit_id
 
     @api.model
     def _make_invoice(self, order, lines):
@@ -43,7 +50,8 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    operating_unit_id = fields.Many2one('operating.unit',
-                                        related='order_id.operating_unit_id',
-                                        string='Operating Unit',
-                                        readonly=True)
+    operating_unit_id = fields.Many2one(
+        comodel_name='operating.unit',
+        related='order_id.operating_unit_id',
+        string='Operating Unit',
+        readonly=True)
