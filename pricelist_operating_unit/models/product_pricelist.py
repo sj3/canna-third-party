ac1e22aacb00c8535a7ba0f48ca63076716037ba
Luc de Meyer <luc.demeyer@noviat.com>
2017-11-22 16:17:57 +0000

    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@349 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

--- pricelist_operating_unit/models/product_pricelist.py
+++ pricelist_operating_unit/models/product_pricelist.py
@@ -22,7 +22,16 @@ class ProductPricelistVersion(models.Model):
 
     operating_unit_id = fields.Many2one(
         comodel_name='operating.unit',
+        related='pricelist_id.operating_unit_id',
         string='Operating Unit',
-        default=lambda self:
-        self.env['res.users'].operating_unit_default_get(self._uid)
-    )
+        readonly=True)
+
+
+class ProductPricelistItem(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    operating_unit_id = fields.Many2one(
+        comodel_name='operating.unit',
+        related='price_version_id.operating_unit_id',
+        string='Operating Unit',
+        readonly=True)
