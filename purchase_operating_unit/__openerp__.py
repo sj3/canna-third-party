0b3fcb7718e61b854a2ba0a7b501e2c109cd4771
Luc de Meyer <luc.demeyer@noviat.com>
2017-11-28 10:39:04 +0000

    remove procurement_operating_unit_dependency
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@358 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

--- purchase_operating_unit/__openerp__.py
+++ purchase_operating_unit/__openerp__.py
@@ -8,13 +8,13 @@
     "name": "Operating Unit in Purchase Orders",
     "summary": "An operating unit (OU) is an organizational entity part of a\
         company",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "author": "Eficent, Serpent Consulting Services Pvt. Ltd.,"
               "Noviat,"
               "Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
     "category": "Purchase Management",
-    "depends": ["purchase", "procurement_operating_unit"],
+    "depends": ["purchase"],
     "license": "AGPL-3",
     "data": [
         "security/purchase_security.xml",
