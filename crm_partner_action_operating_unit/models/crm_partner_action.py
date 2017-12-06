b5aec98856316115d461abccabfb8215fc7ef8a6
Kevin Graveman <k.graveman@onestein.nl>
2017-12-06 14:55:14 +0000

    [MOVE TO BRANCH] Move operating_unit dynamic field domain and company property to operating_unit port to branch operating_unit.
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@366 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

--- crm_partner_action_operating_unit/models/crm_partner_action.py
+++ crm_partner_action_operating_unit/models/crm_partner_action.py
@@ -21,10 +21,8 @@
 ##############################################################################
 
 from openerp import fields, models, api
-from openerp.addons.operating_unit.models import ou_model
 
-
-class CrmPartnerAction(ou_model.OUModel):
+class CrmPartnerAction(models.Model):
     _inherit = 'crm.partner.action'
 
     operating_unit_id = fields.Many2one('operating.unit', 'Operating Unit',
