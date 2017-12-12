9be779b0d0afa6b89962c3a522829a24cb579b4c
Simon Janssens <s.janssens@onestein.nl>
2017-12-12 13:09:25 +0000

    operating_unit_isolation module
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@373 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ operating_unit_isolation/models/base_model.py
@@ -0,0 +1,54 @@
+from openerp import models
+
+
+class BaseModelExtend(models.AbstractModel):
+    _name = 'basemodel.extend'
+
+    def _register_hook(self, cr):
+
+        def _ou_search(
+                self, cr, user, args, offset=0, limit=None,
+                order=None, context=None, count=False, access_rights_uid=None):
+
+            if context is None:
+                context = {}
+            if not args:
+                args = []
+
+            if 'operating_unit_id' in self._fields:
+                if context.get('record', {}).get('_field', False) \
+                   and getattr(
+                       self.pool.get(
+                           context['record']['_name'])._fields[
+                               context['record']['_field']],
+                       'operating_unit_isolation',
+                       False):
+                    isolation = getattr(
+                        self.pool.get(context['record']['_name'])._fields[
+                            context['record']['_field']],
+                        'operating_unit_isolation').split('.')
+                    filter_ou = False
+                    if len(isolation) > 1:
+                        if isolation[0] == context[
+                                'parent_record']['_o2m']:
+                            filter_ou = context[
+                                'parent_record'][isolation[1]]
+                    else:
+                        filter_ou = context['record'][isolation[0]]
+
+                    if filter_ou:
+                        args.append((
+                            'operating_unit_id', 'in', [False, filter_ou]
+                        ))
+
+            return _ou_search.origin(
+                self, cr, user, args, offset=offset, limit=limit,
+                order=order, context=context, count=count,
+                access_rights_uid=access_rights_uid)
+
+        ou_patched = getattr(models.BaseModel, 'ou_patch', None)
+        if not ou_patched:
+            models.BaseModel._patch_method('_search', _ou_search)
+            models.BaseModel.ou_patched = True
+
+        return super(BaseModelExtend, self)._register_hook(cr)
