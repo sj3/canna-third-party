426c952a337fd7b351821bb5ed019bf245da3bc9
Simon Janssens <s.janssens@onestein.nl>
2017-12-12 13:10:50 +0000

    operating_unit_window_action_isolation module
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@374 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ operating_unit_window_action_isolation/models/ir_actions_act_window.py
@@ -0,0 +1,39 @@
+from openerp import models, SUPERUSER_ID
+from openerp.tools.safe_eval import safe_eval
+
+
+class IrActionsAct_window(models.Model):
+    _inherit = 'ir.actions.act_window'
+
+    def read(self, cr, uid, ids, fields=None,
+             context=None, load='_classic_read'):
+        act = res = super(IrActionsAct_window, self).read(
+            cr, uid, ids, fields=fields, context=context, load=load)
+
+        if isinstance(res, list):
+            act = res[0]
+
+        if act.get('res_model', False) and \
+           'operating_unit_id' in self.pool.get(act['res_model'])._fields:
+
+            user_ou = self.pool.get('res.users').read(
+                cr, SUPERUSER_ID, uid, [
+                    'default_operating_unit_id'])[
+                        'default_operating_unit_id']
+
+            if not user_ou:
+                return res
+
+            if act.get('domain', False):
+                try:
+                    act_ctx = safe_eval(act.get('domain', ''))
+                    act_ctx.append(
+                        ('operating_unit_id', 'in', [False, user_ou[0]]))
+                    act['domain'] = str(act_ctx)
+                except ValueError:
+                    pass
+            else:
+                act['domain'] = str([
+                    ('operating_unit_id', 'in', [False, user_ou[0]])])
+
+        return res
