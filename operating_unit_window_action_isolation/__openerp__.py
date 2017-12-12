cb8f268d802abee4cae9516c02d3aa24745576bb
Simon Janssens <s.janssens@onestein.nl>
2017-12-12 13:10:50 +0000

    operating_unit_window_action_isolation module
    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@374 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ operating_unit_window_action_isolation/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Operating Unit Isolation Based on Window Actions",
+    "summary": "An extension to provide isolation for operating units",
+    "version": "8.0.1.0.0",
+    "author": "Odoo Community Association (OCA)",
+    "website": "http://www.onestein.eu",
+    "category": "Accounting and finance",
+    "depends": [
+        "base",
+        "operating_unit",
+    ],
+    "license": "AGPL-3",
+    "data": [
+    ],
+    'installable': True,
+}
