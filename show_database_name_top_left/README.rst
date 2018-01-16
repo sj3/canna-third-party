e188eae05e95038a6ed10c970ce109fa43e7e4fe
Luc de Meyer <luc.demeyer@noviat.com>
2018-01-16 10:35:40 +0000

    git-svn-id: https://10.10.17.25/canna/odoo8-community/trunk@387 d910e14a-ac4a-4a9e-82b0-437c8ed1c4c9

---
+++ show_database_name_top_left/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+==================
+Show database name
+==================
+
+This module shows the Odoo database name in the top left corner.
---
+++ show_database_name_top_left/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2018 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Show Database Name',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Noviat',
+    'category': 'Web',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/menu_secondary.xml',
+    ],
+    'installable': True,
+}
