2921ad0281a0c0611c16165451b05ffd7c18807d
s j <sj@1729.be>
2018-11-14 20:30:25 +0100

    web_optional_m2o_quick_create also disable quick create on FieldMany2ManyTags

--- web_optional_m2o_quick_create/static/src/js/m2o.js
+++ web_optional_m2o_quick_create/static/src/js/m2o.js
@@ -3,7 +3,19 @@ openerp.web_optional_m2o_quick_create = function(instance) {
     init: function(field_manager, node) {
       var self = this;
       this._super(field_manager, node);
-      model = new instance.web.Model('ir.model');
+      if (! _.has(this.options, 'no_create')) {
+        this.options.no_create = true;
+      }
+      if (! _.has(this.options, 'no_quick_create')) {
+        this.options.no_quick_create = true;
+      }
+    }
+  });
+
+  instance.web.form.FieldMany2ManyTags.include({
+    init: function() {
+      var self = this;
+      this._super.apply(this, arguments);
       if (! _.has(this.options, 'no_create')) {
         this.options.no_create = true;
       }
