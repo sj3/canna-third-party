a5a59d4e2d217c3bdc59120d70b293baf32836ef
Kevin Graveman <k.graveman@onestein.nl>
2018-11-16 14:36:29 +0100

    [ADD] web_action_conditionable modified with group conditions

---
+++ web_action_conditionable/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": 'web_action_conditionable',
+    "version": "8.0.0.1.0",
+    "depends": [
+        'base',
+        'web',
+    ],
+    "license": "AGPL-3",
+    'data': ['views/view.xml'],
+    "author": "Cristian Salamea,Odoo Community Association (OCA)",
+    "installable": True,
+}
