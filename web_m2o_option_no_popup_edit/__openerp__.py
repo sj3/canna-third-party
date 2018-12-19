b2cc1b42249f90c609a4c1d974f3a1b077ffb43d
Luc De Meyer <luc.demeyer@noviat.com>
2018-12-19 11:49:46 +0100

    add module web_m2o_option_no_popup_edit

---
+++ web_m2o_option_no_popup_edit/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Noviat nv/sa (www.noviat.com).
+# Copyright (C) 2018 Onestein (http://www.onestein.eu).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Optional Many2One popup edit',
+    'version': '8.0.1.0.0',
+    'author': 'Onestein,Noviat',
+    'license': 'AGPL-3',
+    'category': 'Web',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/assets_backend.xml',
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
---
+++ web_m2o_option_no_popup_edit/static/src/js/m2o_option_no_popup_edit.js
@@ -0,0 +1,27 @@
+openerp.web_m2o_option_no_popup_edit = function (instance) {
+
+    instance.web.form.FieldMany2One.include({
+
+        build_context: function () {
+            var v_context = this._super.apply(this, arguments);
+            if (!_.has(this.options, 'no_popup_edit') || this.options.no_popup_edit) {
+                v_context.add("{'no_popup_edit': 1}");
+            }
+            return v_context;
+        }
+
+    });
+
+    instance.web.form.FormOpenPopup.include({
+
+        show_element: function (model, row_id, context, options) {
+            var ctx = context.eval();
+            if (_.has(ctx, 'no_popup_edit') && ctx.no_popup_edit) {
+                options.readonly = true;
+            };
+            return this._super.apply(this, arguments);
+        }
+
+    });
+
+}
