51172e04e155341259b751065e0885443a5bc200
Kevin Graveman <k.graveman@onestein.nl>
2019-01-10 14:26:41 +0100

    [ADD] Backported version of web_disable_export_group

---
+++ web_disable_export_group/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Web Disable Export Group',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Onestein',
+    'website': 'http://www.onestein.eu',
+    'category': 'Web',
+    'depends': ['web'],
+    'data': [
+        'security/groups.xml',
+        'templates/assets.xml',
+    ],
+    'installable': True,
+}
