2a4ef8b5789fedeeccea0444f39ae49d03d0310e
Kevin Graveman <k.graveman@onestein.nl>
2019-11-19 11:45:01 +0100

    [ADD] git_version

---
+++ git_version/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright Onestein (http://www.onestein.eu).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Git Version Info',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'author': 'Onestein',
+    'depends': [
+        'base',
+        'web',
+    ],
+    'qweb': [
+        "static/src/xml/base.xml",
+    ],
+    'installable': True,
+}
