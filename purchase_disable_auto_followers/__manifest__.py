79b3ad6046449249177e8e987225168547f3ba76
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-22 10:32:12 +0530

    [MIG]purchase_disable_auto_followers: Migrated purchase_disable_auto_followers module in v13.

---
+++ purchase_disable_auto_followers/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2009-2016 Noviat
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Disable Automatic Followers on Purchase Orders",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Noviat, " "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.noviat.com",
+    "category": "Purchase Management",
+    "depends": ["purchase"],
+    "installable": True,
+}
