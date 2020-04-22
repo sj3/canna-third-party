adf6706435714ee72658434bf6336cda8bdf2c96
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2020-04-22 10:59:37 +0530

    [MIG]sale_order_purchase_reference: Migrated sale_order_purchase_reference module in v13.

---
+++ sale_order_purchase_reference/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2015 Onestein BV (www.onestein.eu).
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Sale Order Purchase Reference",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Onestein, " "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.onestein.nl",
+    "category": "Sale",
+    "complexity": "normal",
+    "summary": "Sale Order Purchase Reference",
+    "depends": ["purchase_order_sale_reference",],
+    "data": ["views/sale_order.xml", "views/purchase_order.xml",],
+}
--- sale_order_purchase_reference/models/sale_order.py
+++ sale_order_purchase_reference/models/sale_order.py
@@ -1,79 +1,59 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo, Open Source Management Solution
-#
-#    Copyright (c) 2015 Onestein BV (www.onestein.eu).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 Onestein BV (www.onestein.eu).
+# Copyright (C) 2020-TODAY Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models, _
-from openerp.exceptions import Warning
+from odoo import api, fields, models, _
+from odoo.exceptions import Warning
+
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     purchase_order_ids = fields.Many2many(
-        comodel_name='purchase.order',
-        compute='_compute_purchase_order_ids',
-        search='_search_purchase_order_ids',
-        string='Purchase Orders',
+        comodel_name="purchase.order",
+        compute="_compute_purchase_order_ids",
+        search="_search_purchase_order_ids",
+        string="Purchase Orders",
     )
     purchase_order_count = fields.Integer(
-        compute='_compute_purchase_order_ids',
-        string='# of Sales Order'
-    ) 
-    @api.one
+        compute="_compute_purchase_order_ids", string="# of Sales Order"
+    )
+
     def _compute_purchase_order_ids(self):
-        procs = self.env['procurement.order'].search(
-            [('sale_order_id', '=', self.id),
-             ('state', '!=', 'cancel')
-             ])
-        self.purchase_order_ids = procs.mapped('purchase_id')
+        procs = self.env["procurement.group"].search([("sale_id", "=", self.id),])
+        self.purchase_order_ids = procs.mapped("purchase_id")
         self.purchase_order_count = len(self.purchase_order_ids)
 
     @api.model
     def _search_purchase_order_ids(self, operator, value):
-        if operator == 'in':
+        if operator == "in":
             if isinstance(value, int):
                 value = [value]
-            so_ids = self.env['procurement.order'].search(
-                [('purchase_id', 'in', value),
-                 ('state', '!=', 'cancel')
-             ]).mapped('sale_order_id.id')
-            return [('id', 'in', so_ids)]
+            so_ids = (
+                self.env["procurement.group"]
+                .search([("purchase_id", "in", value)])
+                .mapped("sale_id.id")
+            )
+            return [("id", "in", so_ids)]
         else:
-            raise Warning(_('Unsupported operand for search!'))
+            raise Warning(_("Unsupported operand for search!"))
 
-    @api.multi
     def view_purchase_order(self):
         self.ensure_one()
-
-        action = self.env.ref('sale_order_purchase_reference.purchase_order_action_sales').read()[0]
-        
+        action = self.env.ref(
+            "sale_order_purchase_reference.purchase_order_action_sales"
+        ).read()[0]
         po_ids = [x.id for x in self.purchase_order_ids]
         if po_ids:
-            action.update({
-                    'domain': [('id', 'in', po_ids)],
-                    })
-
+            action.update(
+                {"domain": [("id", "in", po_ids)],}
+            )
             if len(po_ids) == 1:
-                action.update({
-                    'views': [v for v in action.get('views', []) if v[1] == 'form'],
-                    'res_id': po_ids[0],
-                    })
+                action.update(
+                    {
+                        "views": [v for v in action.get("views", []) if v[1] == "form"],
+                        "res_id": po_ids[0],
+                    }
+                )
 
         return action
