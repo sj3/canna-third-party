4cb3163902b8d4f1e674d9ba2f00c31da2065919
Kevin Graveman <k.graveman@onestein.nl>
2020-07-16 15:02:14 +0200

    [8.0][BLACK] chatter_visibility_control

--- chatter_visibility_control/__openerp__.py
+++ chatter_visibility_control/__openerp__.py
@@ -3,16 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Chatter visibility control',
-    'version': '8.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Noviat',
-    'category': 'Social Network',
-    'depends': [
-        'mail',
-    ],
-    'data': [
-        'data/ir_config_parameter.xml',
-    ],
-    'installable': True,
+    "name": "Chatter visibility control",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Noviat",
+    "category": "Social Network",
+    "depends": ["mail",],
+    "data": ["data/ir_config_parameter.xml",],
+    "installable": True,
 }
--- chatter_visibility_control/models/mail_message.py
+++ chatter_visibility_control/models/mail_message.py
@@ -11,7 +11,7 @@ _logger = logging.getLogger(__name__)
 
 
 class MailMessage(models.Model):
-    _inherit = 'mail.message'
+    _inherit = "mail.message"
 
     body_lang = fields.Char()
 
@@ -27,11 +27,13 @@ class MailMessage(models.Model):
     @api.model
     def _message_read_dict(self, message, parent_id=False):
         msg_dict = super(MailMessage, self)._message_read_dict(
-            message, parent_id=parent_id)
+            message, parent_id=parent_id
+        )
         chatter_control_models = safe_eval(
-            self.env['ir.config_parameter']
-            .get_param('chatter_visibility_control') or '[]')
-        if msg_dict['model'] in chatter_control_models:
+            self.env["ir.config_parameter"].get_param("chatter_visibility_control")
+            or "[]"
+        )
+        if msg_dict["model"] in chatter_control_models:
             msg_dict = self._message_control_visibility(message, msg_dict)
         return msg_dict
 
@@ -40,31 +42,33 @@ class MailMessage(models.Model):
 
         removals = {}
         for name, field in model._fields.items():
-            visibility = getattr(field, 'track_visibility', False)
-            c1 = visibility in ['always', 'onchange'] or name in model._track
-            c2 = (hasattr(field, 'track_visibility_groups') and not
-                  self.pool.get('res.users').user_has_groups(
-                      self.env.cr, self.env.uid, field.track_visibility_groups,
-                      self.env.context
-                      )
-                  )
+            visibility = getattr(field, "track_visibility", False)
+            c1 = visibility in ["always", "onchange"] or name in model._track
+            c2 = hasattr(field, "track_visibility_groups") and not self.pool.get(
+                "res.users"
+            ).user_has_groups(
+                self.env.cr,
+                self.env.uid,
+                field.track_visibility_groups,
+                self.env.context,
+            )
             if c1 and c2:
                 removals[name] = field
 
         for f in removals:
-            body_lang = message.body_lang or u'en_US'
-            env = self.with_context({'lang': body_lang}).env
-            field_string = removals[f].get_description(env)['string']
-            for k in ['body', 'body_short']:
+            body_lang = message.body_lang or u"en_US"
+            env = self.with_context({"lang": body_lang}).env
+            field_string = removals[f].get_description(env)["string"]
+            for k in ["body", "body_short"]:
                 body = msg_dict[k][:]
-                if k == 'body':
-                    replace_start = '<b>%s</b>: ' % field_string
-                    replace_end = '</div>'
+                if k == "body":
+                    replace_start = "<b>%s</b>: " % field_string
+                    replace_end = "</div>"
                 else:
-                    replace_start = '<b>%s</b><span>: ' % field_string
-                    replace_end = '</span>'
+                    replace_start = "<b>%s</b><span>: " % field_string
+                    replace_end = "</span>"
                 if replace_start in body:
                     i1 = body.find(replace_start) + len(replace_start)
                     i2 = i1 + msg_dict[k][i1:].find(replace_end)
-                    msg_dict[k] = body[:i1] + _('changed') + body[i2:]
+                    msg_dict[k] = body[:i1] + _("changed") + body[i2:]
         return msg_dict
