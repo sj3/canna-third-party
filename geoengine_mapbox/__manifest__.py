efe9dc0d5657dd96ec0ad5397a1a989dcafed206
Hiren-SerpentCS <hiren.pattani@serpentcs.com>
2020-07-28 15:36:22 +0530

    [ADD] geoengine_mapbox: Added geoengine_mapbox moduel.

---
+++ geoengine_mapbox/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "GeoEngine MapBox",
+    "version": "13.0.1.0.0",
+    "category": "GeoBI",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "license": "AGPL-3",
+    "website": "https://serpentcs.com",
+    "depends": ["geoengine_partner"],
+    "data": [
+        "data/mail_template_data.xml",
+        "views/res_config_settings_views.xml",
+        "views/res_partner_view.xml",
+        "views/template.xml"
+    ],
+    "installable": True,
+    "application": True,
+}
