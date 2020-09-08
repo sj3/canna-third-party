3c6ae245a4c18572071cf4fbbe8521ca443710b2
Chanakya Soni <chanakya.soni@serpentcs.com>
2020-09-08 16:19:33 +0530

    [FIX] Fixed lint errors

--- price_catalog/migrations/13.0.1.2/post-migration.py
+++ price_catalog/migrations/13.0.1.2/post-migration.py
@@ -5,26 +5,26 @@ def fill_product_catalog_prices(env):
     pricelists = env['product.pricelist'].search(['|', ('active', '=', False),
                                                   ('active', '=', True)])
     for pricelist in pricelists:
-        catalog = env['price.catalog'].create(
-            {'active':pricelist.active,
-            'name':pricelist.name,
-            'currency_id':pricelist.currency_id.id,
-            'catalog_type':'sale',
-            'company_id':pricelist.company_id.id
+        catalog = env['price.catalog'].create({
+            'active': pricelist.active,
+            'name': pricelist.name,
+            'currency_id': pricelist.currency_id.id,
+            'catalog_type': 'sale',
+            'company_id': pricelist.company_id.id
         })
         for line in pricelist.item_ids:
-            subcatalog = env['price.subcatalog'].create(
-                {'name':catalog.name,
-                'start_date':line.date_start,
-                'end_date':line.date_end,
-                'catalog_id':catalog.id
+            subcatalog = env['price.subcatalog'].create({
+                'name': catalog.name,
+                'start_date': line.date_start,
+                'end_date': line.date_end,
+                'catalog_id': catalog.id
             })
             if line.product_id:
                 price = pricelist.get_product_price(line.product_id, 1.0, False)
-                items = env['price.catalog.item'].create(
-                    {'product_id':line.product_id.id,
-                    'subcatalog_id':subcatalog.id,
-                    'price':price
+                items = env['price.catalog.item'].create({
+                    'product_id': line.product_id.id,
+                    'subcatalog_id': subcatalog.id,
+                    'price': price
                 })
 
 
