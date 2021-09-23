0995a5be7af3be1f625181b662e240a11ebb5e76
Luc De Meyer <luc.demeyer@noviat.com>
2021-09-23 11:08:49 +0200

    [13.0]fix Issue/2021/0396

--- account_invoice_pay_filter/models/account_payment.py
+++ account_invoice_pay_filter/models/account_payment.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2020 Noviat.
+# Copyright 2009-2021 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
@@ -13,7 +13,10 @@ class AccountPayment(models.Model):
 
     @api.onchange("amount", "currency_id")
     def _onchange_amount(self):
-        res = super()._onchange_amount()
+        ctx = dict(self.env.context, account_invoice_pay_filter_onchange_amount=True)
+        # we need to call super with special context since the super assigns otherwise
+        # the first financial journal returned by search on account.journal.
+        res = super(AccountPayment, self.with_context(ctx))._onchange_amount()
         if not res and not res.get("domain") and not res["domain"].get("journal_id"):
             _logger.error(
                 "Programming Error in _on_change amount. "
@@ -21,21 +24,30 @@ class AccountPayment(models.Model):
             )
             return res
         pj_dom = res["domain"]["journal_id"]
-        company = self.invoice_ids and self.invoice_ids[0].company_id or self.env.company
-        pj_dom.append(("company_id", "=", company.id))
         if self.payment_type == "inbound":
             pj_dom.append(("payment_method_in", "=", True))
         else:
             pj_dom.append(("payment_method_out", "=", True))
         pay_journals = self.env["account.journal"].search(pj_dom)
-        self.journal_id = len(pay_journals) == 1 and pay_journals or False
+        if len(pay_journals) == 1:
+            self.journal_id = pay_journals
         pj_dom = [("id", "in", pay_journals.ids)]
         res["domain"]["journal_id"] = pj_dom
+        _logger.error("_onchange_amount, exit, res=%s", res)
         return res
 
     @api.onchange("journal_id")
     def _onchange_journal(self):
         res = super()._onchange_journal()
+        # The standard Odoo code does not reset currency in case
+        # of company currency. We fix this bug here.
+        # TODO: make PR for odoo.
+        if self.journal_id:
+            currency = (
+                self.journal_id.currency_id or self.journal_id.company_id.currency_id
+            )
+            if self.currency_id != currency:
+                self.currency_id = currency
         if self.journal_id and len(self.invoice_ids) == 1:
             if (
                 self.payment_type == "inbound"
