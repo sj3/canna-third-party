c9eb09077aa6d28194dff664c6a978a90da02d24
Swapnesh <swapnesh.shah@serpentcs.com>
2021-09-29 15:03:23 +0530

    [FIX] git_version: make debug mode work

--- git_version/static/src/js/git_version.js
+++ git_version/static/src/js/git_version.js
@@ -16,7 +16,7 @@ var _t = core._t;
                 var $help = $(QWeb.render("UserMenu.about", {version_info: res}));
                 $help.find('a.oe_activate_debug_mode').click(function (e) {
                     e.preventDefault();
-                    window.location = $.param.querystring( window.location.href, 'debug');
+                    window.location = $.param.querystring( window.location.href, 'debug=1');
                 });
                 new Dialog(this, {
                     size: 'medium',
