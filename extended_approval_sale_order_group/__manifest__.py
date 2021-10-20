64a55714e55960b11d281bb09004e44e791779d0
s j <sj@1729.be>
2021-10-20 06:19:18 +0200

    add: extended_approval_sale_order_group

---
+++ extended_approval_sale_order_group/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright (C) Startx 2021
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Extended Approval Sale Order Group",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Startx",
+    "category": "base",
+    "depends": ["sale_order_group", "base_extended_approval"],
+    "data": ["views/sale_order_group_views.xml"],
+    "installable": True,
+}
