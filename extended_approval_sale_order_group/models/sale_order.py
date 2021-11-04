4aa67a59e0582727821b0859807654809d4efffa
Luc De Meyer <luc.demeyer@noviat.com>
2021-11-04 09:36:23 +0100

    [13.0]add check on sale_order_group config

--- extended_approval_sale_order_group/models/sale_order.py
+++ extended_approval_sale_order_group/models/sale_order.py
@@ -1,11 +1,27 @@
 # Copyright (C) Startx 2021
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import _, models
+from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    def approve_step(self):
+        so_approval_flow = super()._get_applicable_approval_flow()
+        if self.sale_order_group_id:
+            if (
+                so_approval_flow
+                and not self.sale_order_group_id._get_applicable_approval_flow()
+            ):
+                raise UserError(
+                    _(
+                        "Configuration Error: No approval flow defined "
+                        "on Sale Order Group."
+                    )
+                )
+        return super().approve_step()
 
     def _get_applicable_approval_flow(self):
         if self.sale_order_group_id:
