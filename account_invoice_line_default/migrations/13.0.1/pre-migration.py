c7385f12120ff339a702e202d13ca483eb4ec83d
Luc De Meyer <luc.demeyer@noviat.com>
2021-11-25 12:28:06 +0100

    [13.0]I0525 Analytical dimensions not copied to supplier invoice lines when trailing blanks in invoice form view,  field invoice_line_ids context

--- account_invoice_line_default/migrations/13.0.1/pre-migration.py
+++ account_invoice_line_default/migrations/13.0.1/pre-migration.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2020 Noviat.
+# Copyright 2009-2021 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
@@ -8,21 +8,27 @@ def migrate(cr, version):
 
     cr.execute(
         """
-        SELECT id FROM ir_model_fields
-          WHERE name='property_in_inv_account_id'
+        SELECT id, name FROM ir_model_fields
+          WHERE name like 'property_in_inv_acc%'
           AND model='res.partner';
         """
     )
     res = cr.fetchall()
-    field_id = res[0][0]
+    field_id, field_name = res[0]
+    if field_name == "property_in_inv_account_id":
+        fn_in_old = "property_in_inv_account_id"
+        fn_out_old = "property_out_inv_account_id"
+    else:
+        fn_in_old = "property_in_inv_acc"
+        fn_out_old = "property_out_inv_acc"
     cr.execute(  # pylint: disable=E8103
         """
         UPDATE ir_property
           SET name='property_in_inv_account_id', fields_id=%s
-          WHERE name='property_in_inv_account_id';
+          WHERE name='%s';
         UPDATE ir_property
           SET name='property_out_inv_account_id', fields_id=%s
-          WHERE name='property_out_inv_account_id';
+          WHERE name='%s';
         """
-        % (field_id, field_id)
+        % (field_id, fn_in_old, field_id, fn_out_old)
     )
