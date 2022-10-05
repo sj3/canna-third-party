854920ece08dd7e0c15eb9067d374d828d448b2c
Luc De Meyer <luc.demeyer@noviat.com>
2022-10-05 11:00:28 +0200

    [13.0]extended_approval_partner - fix depends + pre-commit

--- extended_approval_partner/__manifest__.py
+++ extended_approval_partner/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright (C) 2020-TODAY SerpentCS Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright (C) Noviat 2022
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Extended Approval Partners",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "license": "AGPL-3",
     "author": "Onestein, Noviat, Serpent Consulting Services Pvt. Ltd.",
     "category": "base",
-    "depends": ["base", "base_automation", "base_extended_approval"],
+    "depends": ["mail", "base_automation", "base_extended_approval"],
     "data": [
         "data/email_template_data.xml",
         "data/base_action_rule_data.xml",
