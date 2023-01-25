06c182ac47d2400cb12fe024fb1af23fd86db956
s j <sj@1729.be>
2023-01-25 14:24:28 +0100

    [ADD] stock_level_extra

---
+++ stock_level_extra/__manifest__.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Level Extra",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Startx",
+    "website": "http://www.startx.be",
+    "category": "Warehouse Management",
+    "depends": ["stock"],
+    "data": [],
+    "installable": True,
+}
