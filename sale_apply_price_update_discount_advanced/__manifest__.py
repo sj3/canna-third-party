d060b6f6d390abe00f24badc359c6240a5a848bb
Luc De Meyer <luc.demeyer@noviat.com>
2023-06-21 13:10:10 +0200

    ITSM-12810 Issue/2023/09065 - discounts not recomputed

---
+++ sale_apply_price_update_discount_advanced/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2009-2023 Noviat
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Recompute Sales Order prices including discounts",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Noviat",
+    "website": "http://www.noviat.com",
+    "category": "Sale",
+    "depends": ["sale_apply_price_update", "sale_discount_advanced"],
+    "installable": True,
+    "auto_install": True,
+}
