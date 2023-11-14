bdf82300865ebecaff65b09af70adac46487dc47
Luc De Meyer <luc.demeyer@noviat.com>
2023-11-14 14:51:20 +0100

    add account_move_delete

---
+++ account_move_delete/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2009-2020 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Delete Journal Entries",
+    "version": "13.0.1.0.0",
+    "category": "Accounting & Finance",
+    "summary": """
+        Delete Journal Entries
+    """,
+    "author": "Noviat",
+    "website": "https://www.noviat.com",
+    "depends": ["account"],
+    "data": ["security/security.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ account_move_delete/models/account_move.py
@@ -0,0 +1,13 @@
+# Copyright 2009-2020 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def unlink(self):
+        if self.env.user.has_group("account_move_delete.group_account_move_delete"):
+            self = self.with_context(dict(self.env.context, force_delete=True))
+        return super(AccountMove, self.with_context()).unlink()
