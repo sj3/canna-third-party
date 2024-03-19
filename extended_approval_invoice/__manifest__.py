b2c737b8d7e01488a7c2060a93a5f39bfc6632ae
Luc De Meyer <luc.demeyer@noviat.com>
2024-03-19 14:34:02 +0100

    Issue/2023/09989 - restart Invoice approval flow at Reset to Draft

--- extended_approval_invoice/__manifest__.py
+++ extended_approval_invoice/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright (C) Onestein
-# Copyright (C) 2020- Noviat
+# Copyright (C) 2020-2024 Noviat
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Extended Approval Invoice",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "license": "AGPL-3",
     "author": "Onestein, Noviat",
     "category": "base",
