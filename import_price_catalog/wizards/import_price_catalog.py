9dd5fa80644cd50ca2de43413c3edbbaa6b2df9a
Anusha <anusha.pp@calin.co.in>
2025-02-17 13:51:10 +0100

    [IMP]import_price_catalog : code optimizations

--- import_price_catalog/wizards/import_price_catalog.py
+++ import_price_catalog/wizards/import_price_catalog.py
@@ -4,7 +4,7 @@
 import base64
 import csv
 import xlrd
-from odoo import models, fields, api
+from odoo import _,api,models, fields
 from odoo.exceptions import UserError
 
 
@@ -18,7 +18,7 @@ class ImportPriceCatalog(models.TransientModel):
     )
     company_id = fields.Many2one("res.company", "Company")
     remove_data = fields.Boolean("Remove All Data")
-    filename = fields.Char("Filename", required=True)
+    filename = fields.Char("File Name", required=True)
 
     @api.model
     def default_get(self, fields_list):
@@ -35,9 +35,9 @@ class ImportPriceCatalog(models.TransientModel):
         if file_extension not in ['csv', 'xlsx']:
             raise UserError("Only CSV or XLSX files are allowed!")
         decoded_data = base64.b64decode(self.data)
-        catalog_item = self.env["price.catalog.item"]
+        price_catalog_item = self.env["price.catalog.item"]
         if self.remove_data:
-            catalog_data_delete = catalog_item.search(
+            catalog_data_delete = price_catalog_item.search(
                 [("subcatalog_id", "=", self.subcatalog_id.id)]
             )
             for item in catalog_data_delete:
@@ -55,16 +55,12 @@ class ImportPriceCatalog(models.TransientModel):
         problem_count = 0
         error_messages= []
         product = self.env["product.product"]
-        catalog_item = self.env["price.catalog.item"]
-        obj_model = self.env["ir.model.data"]
-        rows = list(reader)
-        if len(rows) <= 1:
-            raise UserError("CSV file must contain at least one data row after the header.")
+        price_catalog_item = self.env["price.catalog.item"]
         for index, row in enumerate(reader):
             if index == 0:
                 continue
             product_code, price = row[0].strip(), row[2].strip()
-            product_id = product.search([('default_code', '=', product_code)], limit=1)
+            product_id = product.search([('default_code', '=', str(product_code))], limit=1)
             if product_id:
                 product_id = product_id[0]
                 vals = {
@@ -72,43 +68,29 @@ class ImportPriceCatalog(models.TransientModel):
                     "product_id": product_id.id,
                     "price": price,
                 }
-                catalog_item.create(vals)
+                price_catalog_item.create(vals)
             else:
                 problem_count += 1
                 error_messages.append(f"Product Code: {product_code} not found")
         error_mess = "\n".join(error_messages)
         status_report = "No Problems Occurred" if problem_count == 0 else error_mess
-        model_data_ids = obj_model.search(
-            [("model", "=", "ir.ui.view"), ("name", "=", "data_price_catalog_view_form")]
-        )
-        assert model_data_ids, "Could not find the data.price.catalog.form view."
-        resource_id = model_data_ids.read(fields=["res_id"])[0]["res_id"]
-        return {
-            "view_type": "form",
-            "view_mode": "form",
-            "res_model": "data.price.catalog",
-            "views": [(resource_id, "form")],
-            "type": "ir.actions.act_window",
-            "target": "new",
-            "context": self.with_context(status_report=status_report)._context,
-        }
+        return self._get_action(status_report)
+
 
     def _import_xlsx(self, data,catalog_id):
         """Process XLSX file."""
-        # Open the Excel workbook
         workbook = xlrd.open_workbook(file_contents=data)
         sheet = workbook.sheet_by_index(0)
         if sheet.nrows <= 1:
-            raise UserError("The Excel file must contain at least one data row after the header.")
+            raise  UserError(_("The Excel file must contain at least one data row after the header."))
         problem_count = 0
         error_messages = []
         product = self.env["product.product"]
-        catalog_item = self.env["price.catalog.item"]
-        obj_model = self.env["ir.model.data"]
-        for row_idx in range(1, sheet.nrows):  # Start from row 1 to skip the header
+        price_catalog_item = self.env["price.catalog.item"]
+        for row_idx in range(1, sheet.nrows):
             product_code = sheet.cell(row_idx, 0).value
             price = sheet.cell(row_idx, 2).value
-            product_id = product.search([('default_code', '=', product_code)], limit=1)
+            product_id = product.search([('default_code', '=', str(product_code))], limit=1)
             if product_id:
                 product_id = product_id[0]
                 vals = {
@@ -116,16 +98,22 @@ class ImportPriceCatalog(models.TransientModel):
                     "product_id": product_id.id,
                     "price": price,
                 }
-                catalog_item.create(vals)
+                price_catalog_item.create(vals)
             else:
                 problem_count += 1
                 error_messages.append(f"Product Code: {product_code} not found")
         error_mess = "\n".join(error_messages)
         status_report = "No Problems Occurred" if problem_count == 0 else error_mess
+        return self._get_action(status_report)
+
+    def _get_action(self, status_report):
+        """Return action to open the form view with a status report."""
+        obj_model = self.env["ir.model.data"]
         model_data_ids = obj_model.search(
             [("model", "=", "ir.ui.view"), ("name", "=", "data_price_catalog_view_form")]
         )
-        assert model_data_ids, "Could not find the data.price.catalog.form view."
+        if not model_data_ids:
+            raise UserError(_("Could not find the data.price.catalog.form view"))
         resource_id = model_data_ids.read(fields=["res_id"])[0]["res_id"]
         return {
             "view_type": "form",
@@ -136,7 +124,6 @@ class ImportPriceCatalog(models.TransientModel):
             "target": "new",
             "context": self.with_context(status_report=status_report)._context,
         }
-
 
 class DataPriceCatalog(models.TransientModel):
     _name = "data.price.catalog"
